b0457caccbf54686a7bc1a926d248ead70222216
spacehoboguy <ph.olafsen@gmail.com>
2023-08-07 12:47:05 +0200

    removed icon and added endpoint api request

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 function App() {
+  const [data, setData] = useState()
+  const VITE_FEC_API_KEY = "UPzWEOVhYptFM46107HjwT0OXVrtDYg9HEYFDwMJ"
+  const endpoint = 'https://api.open.fec.gov/v1/candidates/?party=DEM&offset=20api_key=' + VITE_FEC_API_KEY;
+ 
+  console.log(endpoint)
+  useEffect(()=>{
+    axios.get(endpoint)
+    .then((response) => setData(response.data))
+    console.log(data)
+  },[])
+
   return (
     <>
       <h1 className="">Hello world</h1>
