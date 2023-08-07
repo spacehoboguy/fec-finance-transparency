afa2244f716098aaf018c4499d2ca830ca8eacf2
spacehoboguy <ph.olafsen@gmail.com>
2023-08-07 12:49:01 +0200

    api GET request using axios

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 function App() {
   const [data, setData] = useState()
   const VITE_FEC_API_KEY = "UPzWEOVhYptFM46107HjwT0OXVrtDYg9HEYFDwMJ"
-  const endpoint = 'https://api.open.fec.gov/v1/candidates/?party=DEM&offset=20api_key=' + VITE_FEC_API_KEY;
+  const endpoint = 'https://api.open.fec.gov/v1/candidates/?party=DEM&api_key=' + VITE_FEC_API_KEY;
  
   console.log(endpoint)
   useEffect(()=>{
