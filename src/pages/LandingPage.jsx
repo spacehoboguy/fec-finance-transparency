d4d470ff2419ff0d1b4803d24f11242874400e18
spacehoboguy <ph.olafsen@gmail.com>
2023-08-10 14:13:03 +0200

    box sizing border box

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,12 @@
-import { Title } from "@tremor/react";
+import { Title, Text } from "@tremor/react";
 
 export default function LandingPage() {
   return (
-    <main className="h-screen bg-gradient-to-r from-blue-950 from-65% to-100% to-red-600 " >
+    <div className="box-border h-screen w-screen bg-gradient-to-r from-blue-950 from-65% to-100% to-red-600 " >
         <div className="border-solid border-2" justifyContent="center">
-            <Title className=" text-white font" >openPRES</Title>
+            <Title className=" text-white font" >Transparency made accessible</Title>
+            <Text></Text>
         </div>
-    </main>
+    </div>
   )
 }
