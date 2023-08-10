d722327111dc435ac3a7070ac98154a92050fd2c
spacehoboguy <ph.olafsen@gmail.com>
2023-08-10 14:41:13 +0200

    fixes to

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import { Title, Text } from "@tremor/react";
 
 export default function LandingPage() {
   return (
-    <div className="box-border h-screen w-screen bg-gradient-to-r from-blue-950 from-65% to-100% to-red-600 " >
+    <div className="box-border h-screen w-screen  " >
         <div className="border-solid border-2" justifyContent="center">
             <Title className=" text-white font" >Transparency made accessible</Title>
             <Text></Text>
