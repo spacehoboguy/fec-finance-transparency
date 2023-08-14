e8aaa44c46507442d51bb321973de5cf610c623f
spacehoboguy <ph.olafsen@gmail.com>
2023-08-14 13:11:06 +0200

    removed heroicons

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,28 @@
+import { Icon } from "@tremor/react"
+import { useState } from "react"
+
 export default function Search() {
+    const [input, setInput] = useState("")
+
+    function handleInputChange(e) {
+        setInput(e.target.value)
+    }
+
+    function handleSubmit() {
+
+    }
     return (
-        <div className="grid place-content-center">
-        <form>
-            <label>
-                <input type="text" placeholder="Search" />
-            </label>
-        </form>
+        <div className="grid p-2 place-content-center">
+            <form className="appearance-none">
+                <label className="appearance-none">
+                    <input
+                        className="outline-none border-black border-2 rounded-md focus:ring focus:ring-gray-300 focus:placeholder-transparent"
+                        type="text"
+                        placeholder=" name, id... "
+                        onChange={handleInputChange}
+                        onSubmit={handleSubmit} />
+                </label>
+            </form>
         </div>
     )
 }
