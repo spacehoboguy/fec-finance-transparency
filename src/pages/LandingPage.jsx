d4df0cb4e235f07c13108685c88a20c997fb8c79
spacehoboguy <ph.olafsen@gmail.com>
2023-08-14 10:10:08 +0200

    airbnb

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,10 +3,10 @@ import { Title, Text } from "@tremor/react";
 export default function LandingPage() {
   return (
     <div className="box-border h-screen w-screen  " >
-        <div className="border-solid border-2" justifyContent="center">
-            <Title className=" text-white font" >Transparency made accessible</Title>
-            <Text></Text>
-        </div>
+      <div className="border-solid border-2">
+        <Title className=" text-white font" >Transparency made accessible</Title>
+        <Text></Text>
+      </div>
     </div>
   )
 }
