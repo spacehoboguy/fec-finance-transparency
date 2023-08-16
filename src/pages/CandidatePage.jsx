12b2f9a433795e4db780fe65c4b7a73b39840971
spacehoboguy <ph.olafsen@gmail.com>
2023-08-16 13:00:09 +0200

    added abort controller

--- src/pages/CandidatePage.jsx
+++ src/pages/CandidatePage.jsx
@@ -16,16 +16,16 @@ export default function CandidatePage() {
         }).then((res) => {
             res.data && setCandidateInfo(res.data.results);
         }).catch((err) => {
-                if (err.response) {
-                    console.log(err.response.data);
-                    console.log(err.response.status);
-                    console.log(err.response.headers);
-                } else if (err.request) {
-                    console.log(err.request);
-                } else {
-                    console.log('Error', err.message);
-                }
-            })
+            if (err.response) {
+                console.log(err.response.data);
+                console.log(err.response.status);
+                console.log(err.response.headers);
+            } else if (err.request) {
+                console.log(err.request);
+            } else {
+                console.log('Error', err.message);
+            }
+        })
         return () => {
             controller.abort();
         }
