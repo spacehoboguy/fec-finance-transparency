129399705b158f9c00410f24eda65b6216fb3432
spacehoboguy <ph.olafsen@gmail.com>
2023-08-18 12:15:35 +0200

    added title gradient

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,13 +12,13 @@ export default function LandingPage() {
     <div className="box-border h-screen w-screen" >
       <div className="grid place-content-center">
         <div className="grid-cols-2 place-content-center">
-          <div className="w-1 h-24 bg-black"></div>
-          <h1 className="w-96 h-52 text-black text-6xl font-bold tracking-wider" >Transparency <br />made<br /> accessible</h1>
+          <div className="w-1 h-24 bg-red-600"></div>
+          <h1 className="w-100 h-52 font-bold tracking-normal  text-transparent text-6xl bg-clip-text bg-gradient-to-br from-red-600 via-black to-blue-600 " >Transparency <br />made<br /> accessible</h1>
         </div>
         <div className="">
-          <p className="text-xs w-96">Helping voters explore financial information about United States presidential candidates and committees.</p>
+          <p className="text-s w-96">Helping voters explore financial information about United States presidential candidates and committees.</p>
         </div>
-        <nav className="flex justify-evenly pt-6">
+        <nav className="flex justify-evenly pt-10">
           <NavLink to="/explore" className="w-20 text-center border-2 border-black rounded-md shadow-md hover:bg-black hover:shadow-none hover:text-white">Explore</NavLink>
           <NavLink to="/about" className="w-20 text-center border-2 border-black rounded-md shadow-md hover:bg-black hover:shadow-none hover:text-white">About</NavLink>
         </nav>
