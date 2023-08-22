fe4f645132204e7f4b34727b5b5789084edd94d9
spacehoboguy <ph.olafsen@gmail.com>
2023-08-22 14:42:40 +0200

    bug: does not fetch correctly

--- src/components/CandidateFinancialinfo.jsx
+++ src/components/CandidateFinancialinfo.jsx
@@ -1,40 +1,25 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from 'axios';
+import useFetch from "../hooks/useFetch";
 
 function CandidateFinancialinfo() {
 
     const { candId } = useParams();
     const APIKEY = import.meta.env.VITE_API_KEY;
-    const [canFinTot, setCanFinTot] = useState([]); // candidate finance totals - aggregated
-    const [isLoading, setIsLoading] = useState(false);
-    const canFinTotUrl = 'https://api.open.fec.gov/v1/candidates/totals/?page=1&per_page=20&candidate_id=' + candId + '1&election_full=true&sort=-election_year&sort_hide_null=false&sort_null_only=false&sort_nulls_last=false&api_key=' + APIKEY;
-
-
+    const [canFinTot, setCanFinTot] = useState(); // candidate finance totals - aggregated
+    const canFinTotUrl =
+        'https://api.open.fec.gov/v1/candidates/totals/?page=1&per_page=20&candidate_id='
+        + candId
+        + '1&election_full=true&sort=-election_year&sort_hide_null=false&sort_null_only=false&sort_nulls_last=false&api_key='
+        + APIKEY;
+    const { data, isLoading, error } = useFetch(canFinTotUrl)
+    
+    
     useEffect(() => {
-        setIsLoading(true)
-        axios.get(canFinTotUrl).then((res) => {
-            res.data && setCanFinTot(res.data.results);
-           
-            setIsLoading(false)
-
-        }).catch((err) => {
-            setIsLoading(false);
-            
-            if (err.response) {
-                console.log(err.response.data);
-                console.log(err.response.status);
-                console.log(err.response.headers);
-            } else if (err.request) {
-                console.log(err.request);
-            } else {
-                console.log('Error', err.message);
-            }
-        })
-        return () => {
-        }
-    }, [candId])
-           console.log(canFinTot)
+        setCanFinTot(data)
+    }, [data])
+    console.log(canFinTot)
+    console.log(candId)
     return (
         <div>Finance section coming soon
             <div>
