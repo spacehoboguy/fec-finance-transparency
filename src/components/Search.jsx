243001e326e7e16f4fde83e2f1c6ced8bf56e4b4
spacehoboguy <ph.olafsen@gmail.com>
2023-08-22 14:42:10 +0200

    refactoring and a keydown event for search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,32 +7,47 @@ import useFetch from "../hooks/useFetch";
 
 export default function Search() {
     const [input, setInput] = useState("")
-    const debouncedSearchValue = useDebounce(input, 600);
-    const APIKEY = import.meta.env.VITE_API_KEY;
-    const url = 'https://api.open.fec.gov/v1/names/candidates/?q=' + debouncedSearchValue + '&api_key=' + APIKEY;
+    const [searchTerm, setSearchTerm] = useState("")
     const [filteredResult, setFilteredResult] = useState([]);
+    const [isEnterPressed, setIsEnterPressed] = useState(false);
+    const APIKEY = import.meta.env.VITE_API_KEY;
+    const debouncedSearchTerm = useDebounce(searchTerm, 600);
+    const url =
+        searchTerm ?
+            'https://api.open.fec.gov/v1/names/candidates/?q='
+            + debouncedSearchTerm
+            + '&api_key='
+            + APIKEY
+            : null;
     const { data, isLoading, error } = useFetch(url);
 
     useEffect(() => {
-        setFilteredResult(data.filter(cand => {
-            return cand.office_sought === "P";
-        }))
+        if (data && data.length > 0) {
+            setFilteredResult(data.filter((cand) => cand.office_sought === "P"));
+        } else {
+            setFilteredResult([]);
+        }
     }, [data])
+
+    function handleInput(e) {
+        setInput(e.target.value)
+    }
+
+    function handleKeyDown(e) {
+        if (e.key === "Enter") {
+            setSearchTerm(input)
+            setIsEnterPressed(true)
+        }
+    }
 
     return (
         <div className="grid  p-2 place-content-center">
-            <input
-                className="outline-none w-80 mb-10 bg-white border-black border-2 rounded-md hover:ring hover:ring-gray-300 active:ring-gray-400 focus:ring focus:ring-gray-300 focus:placeholder-transparent"
-                type="text"
-                value={input}
-                placeholder=" Search by name"
-                onChange={(e) => setInput(e.target.value)}
-            />
+            <SearchInput handleInput={handleInput} handleKeyDown={handleKeyDown} input={input}/>
             <div className="border-2 bg-gray-200 border-black h-96 w-80 rounded-md shadow-lg">
                 <div className="h-full p-2 rounded-md">
                     <ul className="">
                         {isLoading ? <LoadingSpinner /> : <div>
-                            {filteredResult.length == 0 && <div>No results</div>}
+                            {filteredResult.length === 0 && isEnterPressed && (<div>No results</div>)}
                             {filteredResult.map(({ id, name }) => {
                                 return (
                                     <SearchListItem key={id} candId={id} candName={name} />
@@ -46,16 +61,26 @@ export default function Search() {
         </div>
     )
 }
-
-import React from 'react'
-
 function SearchListItem({ candId, candName }) {
     return (
         <li className="text-sm border-2 m-2 bg-white border-black rounded-md hover:bg-slate-950 hover:text-white hover:cursor-pointer active:ring hover:ring-gray-500">
-            <Link to={`/candidate/${candId}`} candId={candId}>
+            <Link to={`/candidate/${candId}`} >
                 <div className="pl-2 font-semibold"> {candName}</div>
                 <div className="pl-2">Candidate ID: {candId}</div>
             </Link>
         </li>
+    )
+}
+
+function SearchInput({input, handleInput, handleKeyDown}) {
+    return (
+        <input
+        className="outline-none w-80 mb-10 bg-white border-black border-2 rounded-md hover:ring hover:ring-gray-300 active:ring-gray-400 focus:ring focus:ring-gray-300 focus:placeholder-transparent"
+        type="text"
+        value={input}
+        placeholder=" Search by name"
+        onChange={handleInput}
+        onKeyDown={handleKeyDown}
+    />
     )
 }
