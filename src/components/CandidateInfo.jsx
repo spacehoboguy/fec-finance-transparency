234f594ea5824e380d81b4f8b9050a7713877be2
spacehoboguy <ph.olafsen@gmail.com>
2023-09-06 14:08:50 +0200

    added party logo finder

--- src/components/CandidateInfo.jsx
+++ src/components/CandidateInfo.jsx
@@ -3,6 +3,19 @@ import LoadingSpinner from "./LoadingSpinner";
 
 function CandidateInfo({ data, isLoading, error, party }) {
 
+    const setPartySrc = () => {
+        if (party === "REP") {
+            return "/src/img/Republicanlogo.svg.png"
+        } else if (party === "DEM") {
+            return "/src/img/DemocraticLogo.svg.png"
+        } else if (party === "LIB") {
+            return "/src/img/LibertarianLogo.svg.png"
+        } else if (party === "COM") {
+            return "/src/img/CPUSA.svg.png"
+        } else {
+            return "/src/img/notavalible.png"
+        }
+    }
     return (
         <>
             {isLoading ? <LoadingSpinner /> : (
@@ -10,18 +23,19 @@ function CandidateInfo({ data, isLoading, error, party }) {
                     <div className="flex-col py-3">
                         <div className="m-5 p-3 w-fit bg-white rounded-md">
                             <div className="z-10">
-                                <h1 className="font-bold">{data[0].name}</h1>
+                                <h1 className="font-bold">{data.name}</h1>
                                 <div className='text-xs italic'>{data.candidate_id}</div>
                                 <div className="flex">
                                     <h5 className="text-xs w-32">{data.party_full}</h5>
                                     <h5 className="text-xs italic">{data.incumbent_challenge_full}</h5>
                                 </div>
-                                <img
+                                {party !== "IND"
+                                ?<img
                                     className="z-0 w-10 opacity-50"
-                                    src={party === "REP"
-                                        ? "/src/img/Republicanlogo.svg.png"
-                                        : "/src/img/DemocraticLogo.svg.png"}
-                                    alt="Party Logo" />
+                                    src={setPartySrc()} 
+                                    alt="Party Logo" 
+                                    />
+                                :<p className="text-xs">N/A</p>}
                             </div>
                         </div>
                     </div>
